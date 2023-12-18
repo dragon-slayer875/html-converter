8e608d22eca5c8f24fcd0288b34365726a3e88ce
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2023-12-18 12:23:15 +0530

    Setup initial structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="artboard phone-1">320×568</div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <button className="btn btn-primary">Button</button>
+    </>
+  )
+}
+
+export default App
